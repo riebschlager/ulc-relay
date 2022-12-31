cc05b3fc63cae7fa67e4d9f663063cfcd228237e
Chris Riebschlager <riebschlager@gmail.com>
2022-12-30 18:34:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+
 app.get('/', function(request, response) {
   response.send('Welcome to Urban Lumber Company');
 });
